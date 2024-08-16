0ee89ef9acfffdfbf2f3b8abbdc599d7ea76abee
Cristian Sanchez <ederson12f@gmail.com>
2024-08-15 22:52:48 -0500

    one

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request , jsonify
+from flask_cors import CORS
+import base64
+from src.recortar import principal
+
+
+import os
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+cors = CORS(app, resources={r"/subir-imagenes": {"origins": "*"}})
+
+
+@app.route('/subir-imagenes', methods=['POST'])
+def subir_imagenes():
+    try:
+        # Obtener todas las imágenes del formulario
+        images = []
+        for i in range(6):
+            print('images' + str(i+1))
+            data = request.form.get('images' + str(i+1))
+            if data:
+                data_parts = data.split(',')
+                base64_data = data_parts[1]
+
+        #         # Decodificar la imagen base64
+                image_data = base64.b64decode(base64_data)
+        #         # Guardar la imagen en el servidor o realizar cualquier otro procesamiento necesario
+                filename = os.path.join('img', f'images' + str(i+1)+ '.jpg')
+                with open(filename, 'wb') as f:
+                    f.write(image_data)
+        #         # images.append(filename)
+        
+
+        return jsonify(principal()), 200
+    except Exception as e:
+        return 'Error en el servidor', 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+    app.run(debug=True)
+
---
+++ src/recortar.py
@@ -0,0 +1,166 @@
+import cv2 as cv
+import cv2
+import shutil
+
+import numpy as np
+import os
+import random
+
+# import tensorflow as tf
+
+
+def recortar(img):
+    height, width, channels = img.shape
+
+    w = int(width)
+    h = int(height)
+
+    img = img[int(h/2-125):int(h/2+125), int(w/2-125):int(w/2+125)]
+
+    return img
+
+
+def generar(img):
+    image = recortar(img)
+    cv.waitKey(0)
+
+    height, width, channels = image.shape
+
+    # print(height)
+    # print(width)
+
+    widthToRect = 250 // 3  # En este caso, dividimos la imagen en un grid de 3x3
+
+    COLORES = []
+    for i in range(3):
+        for j in range(3):
+            #print("1")
+            start_x = j * widthToRect
+            #print("2")
+
+            end_x = (j + 1) * widthToRect
+
+            start_y = i * widthToRect
+            end_y = (i + 1) * widthToRect
+
+            # Recorta el bloque de la imagen original
+            masked = image[start_y:end_y, start_x:end_x].copy()
+
+            r = 0
+            g = 0
+            b = 0
+
+            for _ in range(100):
+
+                color = masked[random.randint(
+                    0, widthToRect-1), random.randint(0, widthToRect-1)]
+  
+
+                r += int(color[0])
+                g += int(color[1])
+                b += int(color[2])
+
+            r = r//100
+            g = g//100
+            b = b//100
+
+            # print((r, g, b))
+
+
+
+            COLORES.append((r, g, b))
+
+    CUBO = np.zeros((300, 300, 3), np.uint8)
+    index = 0
+
+    for i in range(0, 300, 100):
+        for j in range(0, 300, 100):
+            cv.rectangle(CUBO, (i, j), (i+100, j+100), COLORES[index], -1)
+            index += 1
+
+    rotated_image = cv.rotate(CUBO, cv.ROTATE_90_CLOCKWISE)
+
+    # cv.imshow('Imagen', image)
+    # cv.imshow('Imagen', rotated_image)
+
+    return CUBO
+
+
+def recortar_img_cubo(imgs, name, name2):
+    p = []
+    # print("aaaaaaaaaaaaaaaaaaaaa")
+    small_size = 300//3
+    n = 0
+
+    # Crea la carpeta de nuevo
+
+
+    small_images = []
+
+    for i_m in range(3):
+        for j_m in range(3):
+            start_x = j_m * small_size
+            end_x = (j_m + 1) * small_size
+
+            start_y = i_m * small_size
+            end_y = (i_m + 1) * small_size
+            
+
+            # Recorta el bloque de la imagen original
+            masked = imgs[start_y:end_y, start_x:end_x].copy()
+
+            small_images.append(masked)
+
+    # print(len(small_images))
+    # Guarda y muestra las imágenes más pequeñas
+    n = 0
+    colores = []
+    print("********************************")
+    f = 0
+    for imgsa in small_images:
+        filename = f'img/predecir/img_{name2}_{n}.jpg'
+        n += 1
+        color = categorizar(imgsa, f'img_{name2}_{n}')
+        colores.append(color)
+        # cv.imshow('Imagen', imgsa)
+        # print("--")
+
+        p.append([])
+        cv.imwrite(filename, imgsa)
+    while len(colores) <= 9:
+        colores.append('nn')
+    
+    print("********************************")
+
+    return colores
+
+def categorizar():
+    pass
+def principal():
+    pp = []
+    nombre_carpeta = 'predecir'
+    if os.path.exists('predecir'):
+        # Borra la carpeta y su contenido
+        # os.rmdir(nombre_carpeta)
+        shutil.rmtree(nombre_carpeta)
+    os.mkdir(nombre_carpeta)
+
+    for i in range(6):
+        filename = 'img/images' + str(i+1) + '.jpg'
+        filename2 = 'img/cara' + str(i+1) + '.jpg'
+        cv.imwrite(filename2, generar(cv.imread(filename)))
+
+        img = cv.imread(f'img/cara{i+1}.jpg')
+        # s = recortar(img)
+        # print("suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+        pp.append(recortar_img_cubo(img, f'img/cara{i+1}.jpg', f'cara{i+1}'))
+
+
+    folder_path = 'predecir'  # Reemplaza con la ruta de tu carpeta
+    elements = os.listdir(folder_path)
+    count = len(elements)
+
+    print(f'La carpeta contiene {count} elementos.')
+
+
+    return pp
